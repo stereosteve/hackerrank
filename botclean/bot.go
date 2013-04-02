3304f4cab88f169e86a26578ab4f601416009d57
Steve Perkins <stereosteve@gmail.com>
2013-04-02 00:41:55 -0400

    botclean

---
+++ botclean/bot.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+  "fmt"
+  "bufio"
+  "os"
+  "io"
+)
+
+type Point struct {
+  x int
+  y int
+  cost int
+}
+
+func main() {
+
+  botPos := Point{0,0,0}
+  lowestCost := new(Point)
+
+  reader := bufio.NewReader(os.Stdin)
+
+  line, _ := reader.ReadString('\n')
+  fmt.Sscanf(line, "%d %d", &botPos.x, &botPos.y)
+
+  for x := 0; ; x++ {
+    line, err := reader.ReadString('\n')
+    if err != nil && err != io.EOF { panic(err) }
+    for y := 0; y < len(line); y++ {
+      ch := line[y]
+      if (ch == 'd') {
+        cost := abs(botPos.x - x) + abs(botPos.y - y)
+        point := Point{x, y, cost}
+        if (cost == 0) {
+          fmt.Println("CLEAN")
+          os.Exit(0)
+        } else if lowestCost.cost == 0 || point.cost < lowestCost.cost {
+          lowestCost = &point
+        }
+      }
+    }
+    if err == io.EOF {
+      break
+    }
+  }
+  if botPos.x < lowestCost.x {
+    fmt.Println("DOWN")
+  } else if botPos.x > lowestCost.x {
+    fmt.Println("UP")
+  } else if botPos.y > lowestCost.y {
+    fmt.Println("LEFT")
+  } else if botPos.y < lowestCost.y {
+    fmt.Println("RIGHT")
+  }
+}
+
+func abs(i int) int {
+  if (i < 0) {
+    return -1 * i
+  }
+  return i
+}
